295625ec84fe5c6385ca4399dba90429875e7814
pippps <mat.genilloud@gmail.com>
2026-07-07 11:27:57 +0200

    added error method to Lox.java

--- src/com/craftinginterpreters/lox/Lox.java
+++ src/com/craftinginterpreters/lox/Lox.java
@@ -54,4 +54,12 @@ public class Lox {
     private static void report(int line, String where, String message){
         System.err.println("[line" + line + "] Error" + where + ": " + message);
     }
+
+    static void error(Token token, String message) {
+        if (token.type == TokenType.EOF) {
+            report(token.line, " at end", message);
+        } else {
+            report(token.line, "at '" + token.lexeme + "'", message);
+        }
+    }
 }

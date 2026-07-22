865148061fc5940746af63554a905a4ef650ba6c
pippps <mat.genilloud@gmail.com>
2026-07-22 12:09:32 +0200

    typo

--- src/com/craftinginterpreters/lox/Parser.java
+++ src/com/craftinginterpreters/lox/Parser.java
@@ -98,7 +98,7 @@ public class Parser {
 
     private Expr equality() {
         Expr expr = comparison();
-        while (match(BANG_EQUAL, EQUAL)) {
+        while (match(BANG_EQUAL, EQUAL_EQUAL)) {
             Token operator = previous();
             Expr right = comparison();
             expr = new Expr.Binary(expr, operator, right);

f3e472cb0a1b3994c064201aba2b9bd0c93ec6ab
pippps <mat.genilloud@gmail.com>
2026-07-22 13:35:37 +0200

    change done

--- src/com/craftinginterpreters/lox/Environment.java
+++ src/com/craftinginterpreters/lox/Environment.java
@@ -17,6 +17,9 @@ public class Environment {
 
     Object get(Token name) {
         if (values.containsKey(name.lexeme)) {
+            if(values.get(name.lexeme) == null){
+                throw new RuntimeError(name, "variable not initialized '" + name.lexeme + "'.");
+            }
             return values.get(name.lexeme);
         }
 

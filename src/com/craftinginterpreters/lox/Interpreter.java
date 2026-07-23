ac7ba4cb3a4f15e3e031590960ee3080bf379209
pippps <mat.genilloud@gmail.com>
2026-07-23 12:09:37 +0200

    9.4

--- src/com/craftinginterpreters/lox/Interpreter.java
+++ src/com/craftinginterpreters/lox/Interpreter.java
@@ -19,8 +19,20 @@ public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void>{
 
     @Override
     public Object visitLiteralExpr (Expr.Literal expr) {
-
         return expr.value;
+    }
+
+    @Override
+    public Object visitLogicalExpr (Expr.Logical expr) {
+        Object left = evaluate(expr.left);
+
+        if (expr.operator.type == TokenType.OR) {
+            if(isTruthy(left)) return left;
+        } else {
+            if ((!isTruthy(left))) return left;
+        }
+
+        return evaluate(expr.right);
     }
 
     @Override

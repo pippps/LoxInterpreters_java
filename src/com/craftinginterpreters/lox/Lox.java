413af8dc1942b01204887e7c39660a94ddf716ce
pippps <mat.genilloud@gmail.com>
2026-08-04 10:55:43 +0200

    11.4.2

--- src/com/craftinginterpreters/lox/Lox.java
+++ src/com/craftinginterpreters/lox/Lox.java
@@ -50,6 +50,9 @@ public class Lox {
         List<Stmt> statements = parser.parse();
         if (hadError) return;
 
+        Resolver resolver = new Resolver(interpreter);
+        resolver.resolve(statements);
+
         interpreter.interpret(statements);
     }
 

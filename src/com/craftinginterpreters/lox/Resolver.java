f18a1f0b36eeb9c30ad4ed9cc32eb948a4293b61
pippps <mat.genilloud@gmail.com>
2026-08-08 11:38:52 +0200

    12.4

--- src/com/craftinginterpreters/lox/Resolver.java
+++ src/com/craftinginterpreters/lox/Resolver.java
@@ -177,6 +177,11 @@ public class Resolver implements Expr.Visitor<Void> , Stmt.Visitor<Void> {
 
         return null;
     }
+    @Override
+    public Void visitGetExpr(Expr.Get expr) {
+        resolve(expr.object);
+        return null;
+    }
 
     @Override
     public Void visitGroupingExpr(Expr.Grouping expr) {
@@ -193,6 +198,13 @@ public class Resolver implements Expr.Visitor<Void> , Stmt.Visitor<Void> {
     public Void visitLogicalExpr(Expr.Logical expr) {
         resolve(expr.left);
         resolve(expr.right);
+        return null;
+    }
+
+    @Override
+    public Void visitSetExpr(Expr.Set expr) {
+        resolve(expr.value);
+        resolve(expr.object);
         return null;
     }
 

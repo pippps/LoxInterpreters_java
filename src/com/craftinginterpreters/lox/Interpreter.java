371661510418eaa7a4ce227f92e8d0a11e20aca6
pippps <mat.genilloud@gmail.com>
2026-08-10 10:49:33 +0200

    12.6

--- src/com/craftinginterpreters/lox/Interpreter.java
+++ src/com/craftinginterpreters/lox/Interpreter.java
@@ -67,6 +67,11 @@ public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void>{
         ((LoxInstance)object).set(expr.name, value);
         return value;
     }
+
+    @Override
+    public Object visitThisExpr(Expr.This expr) {
+        return lookUpVariable(expr.keyword, expr);
+    }
     @Override
     public Object visitGroupingExpr(Expr.Grouping expr) {
         return evaluate(expr.expression);

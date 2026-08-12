55d56499d8742a7bcf618f696257f79d13444ddd
pippps <mat.genilloud@gmail.com>
2026-08-12 11:15:42 +0200

    13.3.3

--- src/com/craftinginterpreters/lox/Resolver.java
+++ src/com/craftinginterpreters/lox/Resolver.java
@@ -24,6 +24,7 @@ public class Resolver implements Expr.Visitor<Void> , Stmt.Visitor<Void> {
 
     private enum ClassType {
         NONE,
+        SUBCLASS,
         CLASS
     }
 
@@ -109,6 +110,7 @@ public class Resolver implements Expr.Visitor<Void> , Stmt.Visitor<Void> {
         }
 
         if (stmt.superclass != null) {
+            currentClass = ClassType.SUBCLASS;
             resolve(stmt.superclass);
         }
 
@@ -256,6 +258,11 @@ public class Resolver implements Expr.Visitor<Void> , Stmt.Visitor<Void> {
 
     @Override
     public Void visitSuperExpr(Expr.Super expr) {
+        if (currentClass == ClassType.NONE) {
+            Lox.error(expr.keyword, "Can't use 'super' outside of a class.");
+        } else if (currentClass != ClassType.SUBCLASS) {
+            Lox.error(expr.keyword, "Can't use 'super' in a class with no superclass.");
+        }
         resolveLocal(expr, expr.keyword);
         return null;
     }

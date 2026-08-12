1e9ef5c84e61e00ddb2571d7d144ad50d7c2918c
pippps <mat.genilloud@gmail.com>
2026-08-12 10:07:36 +0200

    13.1

--- src/com/craftinginterpreters/lox/LoxClass.java
+++ src/com/craftinginterpreters/lox/LoxClass.java
@@ -6,9 +6,11 @@ import java.util.Map;
 public class LoxClass implements LoxCallable {
     final String name;
     private final Map<String, LoxFunction> methods;
+    final LoxClass superclass;
 
-    LoxClass(String name, Map<String, LoxFunction> methods) {
+    LoxClass(String name,LoxClass superclass, Map<String, LoxFunction> methods) {
         this.name = name;
+        this.superclass = superclass;
         this.methods = methods;
     }
 
--- src/com/craftinginterpreters/lox/Resolver.java
+++ src/com/craftinginterpreters/lox/Resolver.java
@@ -105,6 +105,13 @@ public class Resolver implements Expr.Visitor<Void> , Stmt.Visitor<Void> {
         currentClass = ClassType.CLASS;
         declare(stmt.name);
         define(stmt.name);
+        if((stmt.superclass != null && stmt.name.lexeme.equals(stmt.superclass.name.lexeme))) {
+            Lox.error(stmt.superclass.name, "A class can't inherit form itself.");
+        }
+
+        if (stmt.superclass != null) {
+            resolve(stmt.superclass);
+        }
 
         beginScope();
         scopes.peek().put("this", true);

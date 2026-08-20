44beeb0d2b01ffc037a6f91698e06bc6c6e12373
pippps <mat.genilloud@gmail.com>
2026-08-20 10:54:47 +0200

    bracket added

--- src/com/craftinginterpreters/lox/Parser.java
+++ src/com/craftinginterpreters/lox/Parser.java
@@ -173,7 +173,7 @@ public class Parser {
         if (!check(RIGHT_PAREN)) {
             do {
                 if (parameters.size() >= 255) {
-                    error(peek(), "Can't jave more than 255 parameters.");
+                    error(peek(), "Can't have more than 255 parameters.");
                 }
                 parameters.add(consume(IDENTIFIER, "Expect parameter name."));
             } while (match(COMMA));
